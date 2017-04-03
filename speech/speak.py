bb8b29ef387113dbf7e87be0bcc709d0935380be
Robert Hilliard <robert.hilliard@klarna.com>
2017-04-03 13:27:09 +0200

    Refactored speak into a class

--- speech/speak.py
+++ speech/speak.py
@@ -1,128 +1,87 @@
-# NOTE: this example requires PyAudio because it uses the Microphone class
-import os
+from __future__ import unicode_literals
 import time
 
-import speech_recognition as sr
-
-
+import os
+import vlc
+import speech_recognition
 from gtts import gTTS
 
 
+class Speak:
+    def __init__(self):
+        self.audio = "tmp.file"
+        self.recog = speech_recognition.Recognizer()
+        self.mic = speech_recognition.Microphone()
 
-def play(path):
-    import vlc
+        self.stop_listening = None
 
-    print "playing..."
-    p = vlc.MediaPlayer(path)
-
-    p.play()
-    time.sleep(0.1)
-    while p.is_playing():
+    def play(self):
+        print "Playing..."
+        p = vlc.MediaPlayer(self.audio)
+        p.play()
         time.sleep(0.1)
+        while p.is_playing():
+            time.sleep(0.1)
 
+    @staticmethod
+    def remove_if_exists(path):
+        if os.path.exists(path):
+            os.remove(path)
 
-def play_pyglet(path):
-    """
+    def speak(self, string):
+        """
+        Uses a google rest api
+        :param string:
+        :return:
+        """
+        if not string:
+            print('No input...')
+            return
 
-    :param path:
-    :return:
-    """
-    import pyglet
+        print(string)
+        tts = gTTS(text=string, lang='en')
 
-    print "playing..."
-    song = pyglet.media.load(path)
-    song.play()
-    pyglet.app.run()
+        print("Saving...")
+        Speak.remove_if_exists(self.audio)
+        tts.save(self.audio)
 
+        self.play()
 
-def play_wave(path):
-    """
+    def start(self):
+        with self.mic as source:
+            self.recog.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
 
-    :param path:
-    :return:
-    """
-    import wave
-    import pyaudio
+        self.stop_listening = self.recog.listen_in_background(self.mic, self._callback)
 
-    CHUNK = 1024
+    def stop(self):
+        if self.stop_listening:
+            self.stop_listening()
 
-    wf = wave.open(path, 'rb')
-    # instantiate PyAudio (1)
-    p = pyaudio.PyAudio()
-    # open stream (2)
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
+    def _callback(self, recog, audio):
+        """
 
-    # read data
-    data = wf.readframes(CHUNK)
+        """
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            # print("Speech Recognition thinks you said '" + recognizer.recognize_sphinx(audio) + "'")
+            # print("Speech Recognition thinks you said '" + recognizer.recognize_sphinx(audio) + "'")
 
-    # play stream (3)
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(CHUNK)
+            audio_string = recog.recognize_sphinx(audio)
+            self.speak(audio_string)
+            # print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+        except speech_recognition.UnknownValueError:
+            print("Speech Recognition could not understand audio")
+        except speech_recognition.RequestError as e:
+            print("Could not request results from Speech Recognition service; {0}".format(e))
 
-    # stop stream (4)
-    stream.stop_stream()
-    stream.close()
+s = Speak()
+s.start()
 
-    # close PyAudio (5)
-    p.terminate()
-
-def speak(audioString):
-    """
-    Uses a google rest api
-    :param audioString:
-    :return:
-    """
-    print(audioString)
-    tts = gTTS(text=audioString, lang='en')
-    path = "audio.mp3"
-    print "saving..."
-    tts.save(path)
-    play(path)
-
-    #os.system("mpg321 audio.mp3")
-
-# this is called from the background thread
-def callback(recognizer, audio):
-    """
-
-    :param recognizer:
-    :param audio:
-    :return:
-    """
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        #print("Speech Recognition thinks you said '" + recognizer.recognize_sphinx(audio) + "'")
-        #print("Speech Recognition thinks you said '" + recognizer.recognize_sphinx(audio) + "'")
-
-        audio_string = recognizer.recognize_sphinx(audio)
-        speak(audio_string)
-        # print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Speech Recognition service; {0}".format(e))
-
-r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
-
-# start listening in the background (note that we don't have to do this inside a `with` statement)
-stop_listening = r.listen_in_background(m, callback)
-# `stop_listening` is now a function that, when called, stops background listening
-
-# do some other computation for 5 seconds, then stop listening and keep doing other computations
-for _ in range(50):
-    time.sleep(0.1) # we're still listening even though the main thread is doing other things
-
-stop_listening() # calling this function requests that the background listener stop listening
-
+index = 0
 while True:
+    if index % 10 == 0:
+        print "Still listening..."
     time.sleep(0.1)
+    index += 1
